7575c169cfa3da1693c8511b3e1f3737b70dd185
renuvardhan007 <rk1759@srmist.edu.in>
2026-04-06 11:23:36 +0530

    UC5: enhanced for-loop implementation

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,23 @@
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+
+        System.out.println("Hello, " + nameBuilder + "!");
+    }
+}
