45b17d394a97b1325c8e468670f24a9e8be0b4f8
renurvardhan <renuvardhan.kk@gmail.com>
2026-05-04 23:46:00 +0530

    UC10: draw detection logic

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,44 +1,22 @@
 public class TicTacToe {
 
     static char[][] board = {
-        {'X', 'X', 'X'},
-        {'-', 'O', '-'},
-        {'O', '-', 'O'}
+        {'X', 'O', 'X'},
+        {'X', 'O', 'O'},
+        {'O', 'X', 'X'}
     };
 
     public static void main(String[] args) {
-        System.out.println(hasWon('X'));
+        System.out.println(isDraw());
     }
 
-    static boolean hasWon(char symbol) {
-
-        // rows
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol &&
-                board[i][1] == symbol &&
-                board[i][2] == symbol)
-                return true;
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-')
+                    return false;
+            }
         }
-
-        // columns
-        for (int i = 0; i < 3; i++) {
-            if (board[0][i] == symbol &&
-                board[1][i] == symbol &&
-                board[2][i] == symbol)
-                return true;
-        }
-
-        // diagonals
-        if (board[0][0] == symbol &&
-            board[1][1] == symbol &&
-            board[2][2] == symbol)
-            return true;
-
-        if (board[0][2] == symbol &&
-            board[1][1] == symbol &&
-            board[2][0] == symbol)
-            return true;
-
-        return false;
+        return true;
     }
 }
